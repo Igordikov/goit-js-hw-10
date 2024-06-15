cdd382b051919ed832e80eb2ee0143eabba5413a
Igor <igorivandikov87@gmail.com>
2024-06-15 22:43:52 +0300

    hw-10

---
+++ src/js/snackbar.js
@@ -0,0 +1,65 @@
+
+// import
+
+import iziToast from "iziToast";
+
+// =================================================================================
+
+
+
+// finding elements
+
+document.querySelector('.form').addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+const delayInput = document.querySelector('[name="delay"]');
+const stateInput = document.querySelector('[name="state"]:checked');
+
+// =================================================================================
+
+
+
+// converting a value to a number
+    
+    const delay = parseInt(delayInput.value, 10);
+
+    // =================================================================================
+    
+    
+    // Creating promise and function from iziToast
+
+try {
+        const result = await new Promise((resolve, reject) => {
+            setTimeout(() => {
+               
+                if (stateInput.value === 'fulfilled') {
+                    resolve(`✅ Fulfilled promise in ${delay} ms`);
+                } else {
+                    reject(`❌ Rejected promise in ${delay} ms`);
+                }
+            }, delay);
+            
+            delayInput.value = '';
+        });
+
+
+        iziToast.settings({
+            icon: '',
+        });
+
+
+
+
+        iziToast.success({
+            message: result,
+            position: 'topRight',
+        });
+    } catch (error) {
+        iziToast.error({
+            message: error,
+            position: 'topRight',
+        });
+    }
+});
+          
+// =================================================================================
